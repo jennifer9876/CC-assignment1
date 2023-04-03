5b1dadc852b0d586b3948134e11b968dc4aa6c10
avanijagani96 <s3827022@student.rmit.edu.au>
2023-04-03 23:39:13 +1000

    minor change

--- src/App.js
+++ src/App.js
@@ -9,73 +9,63 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { removeLocalUser } from "../src/AWS/route";
 
 function App() {
-    //useState set to grab user in localstorage to keep user logged in
-    const [loggedInUser, setLoggedInUser] = useState(
-        localStorage.getItem("loggedInUser")
-    );
+  //useState set to grab user in localstorage to keep user logged in
+  const [loggedInUser, setLoggedInUser] = useState(
+    localStorage.getItem("loggedInUser")
+  );
 
-    //centralise login and logout
-    const onLogin = (email) => {
-        setLoggedInUser(email);
-        localStorage.setItem("loggedInUser", email);
-    };
-    const onLogout = (email) => {
-        removeLocalUser();
-    };
+  //centralise login and logout
+  const onLogin = (email) => {
+    setLoggedInUser(email);
+    localStorage.setItem("loggedInUser", email);
+  };
+  const onLogout = (email) => {
+    removeLocalUser();
+  };
 
-    function onDelete(postId) {
-        window.localStorage.removeItem("post_" + postId);
-        let postIds = window.localStorage.getItem("postIds");
-        postIds = JSON.parse(postIds);
+  function onDelete(postId) {
+    window.localStorage.removeItem("post_" + postId);
+    let postIds = window.localStorage.getItem("postIds");
+    postIds = JSON.parse(postIds);
 
-        let loginInfo = window.localStorage.getItem(loggedInUser);
-        loginInfo = JSON.parse(loginInfo);
+    let loginInfo = window.localStorage.getItem(loggedInUser);
+    loginInfo = JSON.parse(loginInfo);
 
-        let idx = postIds.indexOf(postId);
-        let newPostIds = [...postIds];
-        newPostIds.splice(idx, 1);
-        window.localStorage.setItem("postIds", JSON.stringify(newPostIds));
+    let idx = postIds.indexOf(postId);
+    let newPostIds = [...postIds];
+    newPostIds.splice(idx, 1);
+    window.localStorage.setItem("postIds", JSON.stringify(newPostIds));
 
-        let userPostIds = loginInfo.userPostIds;
-        let userPostIdx = userPostIds.indexOf("post_" + postId);
-        let newUserPostIds = [...userPostIds];
-        newUserPostIds.splice(userPostIdx, 1);
-        loginInfo.userPostIds = newUserPostIds;
-        window.localStorage.setItem(loggedInUser, JSON.stringify(loginInfo));
-    }
+    let userPostIds = loginInfo.userPostIds;
+    let userPostIdx = userPostIds.indexOf("post_" + postId);
+    let newUserPostIds = [...userPostIds];
+    newUserPostIds.splice(userPostIdx, 1);
+    loginInfo.userPostIds = newUserPostIds;
+    window.localStorage.setItem(loggedInUser, JSON.stringify(loginInfo));
+  }
 
-    return ( <
-            BrowserRouter >
-            <
-            Routes >
-            <
-            Route path = '/'
-            element = { < LoginPage onLogin = { onLogin }
-                />} / >
-                <
-                Route path = '/signuppage'
-                element = { < SignUpPage / > }
-                /> <
-                Route
-                path = '/homepage'
-                element = { < HomePage loggedInUser = { loggedInUser }
-                    onLogout = { onLogout }
-                    />} /
-                    >
-                    <
-                    Route
-                    path = '/profilepage'
-                    element = { <
-                        ProfilePage
-                        loggedInUser = { loggedInUser }
-                        onLogout = { onLogout }
-                        onDelete = { onDelete }
-                        />
-                    }
-                    /> <
-                    /Routes> <
-                    /BrowserRouter>
-                );
-            }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage onLogin={onLogin} />} />
+        <Route path='/signuppage' element={<SignUpPage />} />
+        <Route
+          path='/homepage'
+          element={<HomePage loggedInUser={loggedInUser} onLogout={onLogout} />}
+        />
+        <Route
+          path='/profilepage'
+          element={
+            <ProfilePage
+              loggedInUser={loggedInUser}
+              onLogout={onLogout}
+              onDelete={onDelete}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-            export default App;
+export default App;
