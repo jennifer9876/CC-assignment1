0991e5c224374ddf192e1def782ae40f178559a7
ajagani@bunnings.com.au <ajagani@bunnings.com.au>
2024-10-02 23:05:45 +1000

    added a test comment

--- src/CommonComponents/Alert.js
+++ src/CommonComponents/Alert.js
@@ -1,8 +1,9 @@
-import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
+// This a test comment
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
 });
@@ -13,7 +14,7 @@ export default function Alerts(props) {
   return (
     <Stack>
       <Snackbar open={open} autoHideDuration={1000} onClose={onClose}>
-        <Alert onClose={onClose} sx={{ width: "100%" }}>
+        <Alert onClose={onClose} sx={{ width: '100%' }}>
           {customText}
         </Alert>
       </Snackbar>
